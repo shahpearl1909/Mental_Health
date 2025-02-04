5dbfefda57ca6fa97746d53c34ae94b0f95d0d58
shahpearl1909 <87839222+shahpearl1909@users.noreply.github.com>
2025-02-03 21:01:44 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-// Toggle the navigation menu when the hamburger button is clicked
-const hamburgerButton = document.querySelector('.hamburger-menu');
-const navbarLinks = document.querySelector('.navbar-links');
 
-// Event listener to toggle the 'active' class
-hamburgerButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active');
-});
-
+//Hamburger Toogle Navigation
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    const navbarLinks = document.querySelector('.navbar-links');
+    const navLinks = document.querySelectorAll('.navbar-links a'); // Select all links
+  
+    if (hamburgerButton && navbarLinks) {
+      hamburgerButton.addEventListener('click', () => {
+        navbarLinks.classList.toggle('active');
+      });
+  
+      // Close the hamburger menu when a navigation link is clicked
+      navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+          navbarLinks.classList.remove('active'); // Hide the menu
+        });
+      });
+    } else {
+      console.error('Elements not found in the DOM');
+    }
+  });
+  
 
 // Mood Tracker Logic
 let moodHistory = [];
